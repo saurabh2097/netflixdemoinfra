9b49a446d0648c4ebbee2a2810fc6d71ccbb0238
saurabh2097 <saurabhchimankar97@gmail.com>
2025-03-27 19:46:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+key_name = "saurabhteraformkeypair"
+vpc_security_group_ids = ["sg-0421382adccde5d20"]
 tags = {
 Name = var.instance_names[count.index]
 }
